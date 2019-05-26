e8ecbe4ffc42d4fc2784639e77b83960fcbdb7eb
7ark <funnyman1120@hotmail.com>
2019-05-26 17:40:25 -0500

    VitalChat general implementation

--- VitalChat/src/main/java/io/github/caprapaul/vitalchat/commands/ChatCommands.java
+++ VitalChat/src/main/java/io/github/caprapaul/vitalchat/commands/ChatCommands.java
@@ -9,6 +9,10 @@ import me.lucko.luckperms.api.Contexts;
 import me.lucko.luckperms.api.Group;
 import me.lucko.luckperms.api.User;
 import me.lucko.luckperms.api.context.ContextSet;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -18,6 +22,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -52,6 +57,7 @@ public class ChatCommands extends BetterCommandExecutor implements Listener
     {
         playerChatInfo = new HashMap<String, ArrayList<String>>();
         loadCommands(this, plugin);
+        loadConfig();
 
         this.chatInfoFile = new File(this.plugin.getDataFolder(), "chatInfo.yml");
         this.chatInfoConfig = YamlConfiguration.loadConfiguration(this.chatInfoFile);
@@ -62,6 +68,13 @@ public class ChatCommands extends BetterCommandExecutor implements Listener
         @SuppressWarnings("unchecked")
         Iterable<Player> onlinePlayers = (Iterable<Player>) Bukkit.getServer().getOnlinePlayers();
         loadAllChatInfo(onlinePlayers);
+    }
+
+    private void loadConfig()
+    {
+        plugin.getConfig().addDefault("nick-max-length", 10);
+        plugin.getConfig().options().copyDefaults(true);
+        plugin.saveConfig();
     }
 
     @Override
@@ -109,6 +122,11 @@ public class ChatCommands extends BetterCommandExecutor implements Listener
 
         ArrayList<String> chatInfoObjects = (ArrayList<String>)chatInfoConfig.get("chatInfo." + playerUUID);
 
+        if(chatInfoObjects == null)
+        {
+            chatInfoObjects = new ArrayList<String>();
+        }
+
         while (chatInfoObjects.size() < ChatInfoSettings.length)
         {
             String current = ChatInfoSettings[chatInfoObjects.size()];
@@ -143,6 +161,44 @@ public class ChatCommands extends BetterCommandExecutor implements Listener
         this.playerChatInfo.remove(playerUUID);
     }
 
+    @BetterCommand(name = "setnick")
+    public void setnick(CommandSender commandSender, String[] args, String commandLabel)
+    {
+        if (!(commandSender instanceof Player))
+        {
+            commandSender.sendMessage(plugin.prefix + ChatColor.RED + "Error: The console can't change it's nickname");
+            return;
+        }
+
+        if(args.length == 0)
+        {
+            commandSender.sendMessage(plugin.prefix + ChatColor.RED + "Error: Need to specify a player and nickname");
+        }
+
+        @SuppressWarnings("unchecked")
+        Iterable<Player> onlinePlayers = (Iterable<Player>) Bukkit.getServer().getOnlinePlayers();
+
+        Player player = null;
+        //Try to find player based on name passed
+        String playerName = args[0];
+        for (Player p : onlinePlayers)
+        {
+            if(playerName.equals(p.getDisplayName()))
+            {
+                player = p;
+                break;
+            }
+        }
+
+        if(player == null)
+        {
+            commandSender.sendMessage(plugin.prefix + ChatColor.RED + "Error: Could not find player");
+            return;
+        }
+
+        String newName = CombineStrings(args, 1);
+        SetPlayerName(player, newName, commandSender);
+    }
 
     @BetterCommand(name = "nick")
     public void nick(CommandSender commandSender, String[] args, String commandLabel)
@@ -153,21 +209,132 @@ public class ChatCommands extends BetterCommandExecutor implements Listener
             return;
         }
 
+        Player player = (Player) commandSender;
+        String playerUUID = player.getUniqueId().toString();
+
         if(args.length == 0)
         {
-            commandSender.sendMessage(plugin.prefix + ChatColor.RED + "Error: You must specify your new nickname!");
+            commandSender.sendMessage(plugin.prefix + "Nickname reset to default");
+            ArrayList<String> array = playerChatInfo.get(playerUUID);
+            array.set(0, player.getDisplayName());
+
+            playerChatInfo.put(playerUUID, array);
+
+            return;
+        }
+
+        String newName = CombineStrings(args, 0);
+        SetPlayerName(player, newName, commandSender);
+    }
+
+    private void SetPlayerName(Player player, String newName, CommandSender commandSender)
+    {
+        String playerUUID = player.getUniqueId().toString();
+        int nameLength = getTrueNameLength(newName);
+
+        if(nameLength == 0)
+        {
+            commandSender.sendMessage(plugin.prefix + ChatColor.RED + "Error: Nickname must be at least one character long!");
+            return;
+        }
+
+        int duplicates = CheckForDuplicate(newName);
+        if(duplicates != 0)
+        {
+            newName += "(" + (duplicates + 1) + ")";
+            commandSender.sendMessage(plugin.prefix + ChatColor.RED + " Someone already has this name! Adding a (" + (duplicates + 1) + ")");
+        }
+
+        ArrayList<String> array = playerChatInfo.get(playerUUID);
+        array.set(0, newName);
+
+        playerChatInfo.put(playerUUID, array);
+
+        commandSender.sendMessage(plugin.prefix + "Name has been changed to " + EvaluateStringForColors(newName));
+        if(((Player)commandSender) != player)
+        {
+            player.sendMessage(plugin.prefix + "Your name has been changed to " + EvaluateStringForColors(newName) + ChatColor.WHITE + " by " + ((Player)commandSender).getDisplayName());
+        }
+    }
+
+    private int getTrueNameLength(String finalName)
+    {
+        int count = 0;
+        boolean skipNext = false;
+        char[] chars = finalName.toCharArray();
+        for (int i = 0; i < chars.length; i++)
+        {
+            if(skipNext)
+            {
+                skipNext = false;
+                continue;
+            }
+            else if(chars[i] == '&')
+            {
+                skipNext = true;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int CheckForDuplicate(String finalName)
+    {
+        int currentCount = 0;
+        for (ArrayList<String> data : playerChatInfo.values())
+        {
+            if(data.get(0) == finalName)
+            {
+                currentCount++;
+            }
+        }
+        return currentCount;
+    }
+
+    @BetterCommand(name = "chatcolor")
+    public void chatcolor(CommandSender commandSender, String[] args, String commandLabel)
+    {
+        if (!(commandSender instanceof Player))
+        {
+            commandSender.sendMessage(plugin.prefix + ChatColor.RED + "Error: The console can't change it's chat color");
+            return;
+        }
+
+        ChatColor[] colors = ChatColor.values();
+        if(args.length == 0)
+        {
+            String finalMessage = "";
+            for (ChatColor color : colors)
+            {
+                finalMessage += ChatColor.translateAlternateColorCodes('&', "&" + Character.toString(color.getChar())) + "&" + color.getChar();
+                finalMessage += ChatColor.translateAlternateColorCodes('&',"&r");
+            }
+            commandSender.sendMessage(finalMessage);
             return;
         }
 
         Player player = (Player) commandSender;
         String playerUUID = player.getUniqueId().toString();
 
-        String newName = CombineStrings(args);
+        boolean inputValid = args.length == 1 && args[0].toCharArray().length == 2 && args[0].toCharArray()[0] == '&' && ChatColor.getByChar(args[0].toCharArray()[1]) != null;
 
-        ArrayList<String> array = playerChatInfo.get(playerUUID);
-        array.set(0, newName);
+        if(inputValid)
+        {
+            String newColor = args[0];
 
-        playerChatInfo.put(playerUUID, array);
+            ArrayList<String> array = playerChatInfo.get(playerUUID);
+            array.set(1, newColor);
+
+            playerChatInfo.put(playerUUID, array);
+
+            commandSender.sendMessage("Your chat color is now " + ChatColor.getByChar(newColor) + ChatColor.getByChar(newColor).name());
+        }
+        else {
+            commandSender.sendMessage(plugin.prefix + ChatColor.RED + "Error: Invalid input (Example: /chatcolor &7");
+        }
     }
 
     @EventHandler
@@ -182,22 +349,34 @@ public class ChatCommands extends BetterCommandExecutor implements Listener
         this.unloadPlayerChatInfo(event.getPlayer());
     }
 
-    @EventHandler
+    @EventHandler(priority= EventPriority.HIGHEST)
     public  void onPlayerChat(AsyncPlayerChatEvent chatEvent)
     {
         Player p = chatEvent.getPlayer();
         String pId = p.getUniqueId().toString();
         String prefix = EvaluateStringForColors(GetPlayerPrefix(p));
 
-        String playerName = EvaluateStringForColors(playerChatInfo.get(pId).get(0));
-        String chatMessage = EvaluateStringForColors(playerChatInfo.get(pId).get(1) + chatEvent.getMessage());
-        chatEvent.setFormat(prefix + ChatColor.GRAY + playerName + ChatColor.GRAY + ": " + chatMessage);
+        String playerName =  EvaluateStringForColors(playerChatInfo.get(pId).get(0));
+
+        TextComponent mainComponent = new TextComponent(prefix + ChatColor.GRAY );
+        TextComponent subComponent = new TextComponent( playerName );
+        subComponent.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( p.getDisplayName() ).create() ) );
+        mainComponent.addExtra( subComponent );
+
+        String chatMessage = EvaluateStringForColors(": " + playerChatInfo.get(pId).get(1) + chatEvent.getMessage());
+        mainComponent.addExtra( chatMessage );
+
+        for (Player player: chatEvent.getRecipients())
+        {
+            player.spigot().sendMessage(mainComponent);
+        }
+        chatEvent.setCancelled(true);
     }
 
-    private String CombineStrings(String[] strings)
+    private String CombineStrings(String[] strings, int startIndex)
     {
         String result = "";
-        for (int i = 0; i < strings.length; i++) {
+        for (int i = startIndex; i < strings.length; i++) {
             result += strings[i];
         }
         return result;
@@ -212,37 +391,38 @@ public class ChatCommands extends BetterCommandExecutor implements Listener
 
     private String EvaluateStringForColors(String text)
     {
-        String finalText = text;
-        if(text == "null")
-        {
-            return "";
-        }
-        if(text.contains("&"))
-        {
-            finalText = "";
-            boolean checkNext = false;
-            for (int i = 0; i < text.length(); i++)
-            {
-                char current = text.toCharArray()[i];
-                if(!checkNext)
-                {
-                    if(current == '&')
-                    {
-                        checkNext = true;
-                    }
-                    else
-                    {
-                        finalText += current;
-                    }
-                }
-                else
-                {
-                    finalText += ChatColor.getByChar(current);
-                    checkNext = false;
-                }
-            }
-        }
-
-        return finalText;
+        return ChatColor.translateAlternateColorCodes('&', text);
+        //String finalText = text;
+        //if(text == "null")
+        //{
+        //    return "";
+        //}
+        //if(text.contains("&"))
+        //{
+        //    finalText = "";
+        //    boolean checkNext = false;
+        //    for (int i = 0; i < text.length(); i++)
+        //    {
+        //        char current = text.toCharArray()[i];
+        //        if(!checkNext)
+        //        {
+        //            if(current == '&')
+        //            {
+        //                checkNext = true;
+        //            }
+        //            else
+        //            {
+        //                finalText += current;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            finalText += ChatColor.getByChar(current);
+        //            checkNext = false;
+        //        }
+        //    }
+        //}
+//
+        //return finalText;
     }
 }
